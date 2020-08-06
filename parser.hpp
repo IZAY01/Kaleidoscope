1cc70fd86573d4f9c060adb2409f03fb46d54c3f
IZAY01 <1906826229@qq.com>
2020-08-06 23:14:39 +0800

    add parser

--- parser.hpp
+++ parser.hpp
@@ -30,4 +30,13 @@ public:
 };
 
 
+class CallExprAST : public ExprAST {
+    std::string callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+    CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args)
+        : Callee(Callee), Args(std::move(Args)) {}
+}
+
 
