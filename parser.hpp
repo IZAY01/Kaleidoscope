f22962e51ba430f71f9937294168efbf4dfb3547
IZAY01 <1906826229@qq.com>
2020-08-18 10:39:31 +0800

    finish AST

--- parser.hpp
+++ parser.hpp
@@ -37,6 +37,28 @@ class CallExprAST : public ExprAST {
 public:
     CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args)
         : Callee(Callee), Args(std::move(Args)) {}
-}
+};
+
+
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &name, std::vector<std::string> Args)
+        : Name(name), Args(std::move(Args)) {}
+    const std::string &getName() const { return Name; }
+};
+
+
+class FunctionAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
+        : Proto(std::move(Proto)), Body(std::move(Body)) {} 
+};
+
 
 
